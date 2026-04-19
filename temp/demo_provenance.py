fd10af2713f1ae0b04f0c305d1abfa52b9c26a8e
Kelvin Kabute <nanayaw@live.com>
2026-04-19 21:49:48 +0000

    chore: test provenance hook (fixed, runpy)

---
+++ temp/demo_provenance.py
@@ -0,0 +1,5 @@
+# 
+# Author: Kelvin Kabute
+# Last-updated: 2026-04-19
+
+print("demo from provenance hook")
---
+++ temp/sample.py
@@ -0,0 +1,10 @@
+# 
+# Author: Kelvin Kabute
+# Last-updated: 2026-04-19
+
+# 
+# Author: test-user
+# Last-updated: 2026-04-19
+
+print("hello world")
+print("hello world")
